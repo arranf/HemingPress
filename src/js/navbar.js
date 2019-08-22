8faf6497683f922804f273a48203d660f84f3c49
Arran <arran@arranfrance.com>
2019-08-22 22:32:56 +0100

    Add navbar swiping

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,28 +1,45 @@
-//  onTouchStart(e) {
-//     this.touchStart = {
-//       x: e.changedTouches[0].clientX,
-//       y: e.changedTouches[0].clientY
-//     }
-//   },
-//   onTouchEnd(e) {
-//     const dx = e.changedTouches[0].clientX - this.touchStart.x
-//     const dy = e.changedTouches[0].clientY - this.touchStart.y
-//     if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
-//       if (dx > 0 && this.touchStart.x <= 80) {
-//         this.toggleSidebar(true)
-//       } else {
-//         this.toggleSidebar(false)
-//       }
-//     }
-//   }
+let touchStart = {};
+
+function onTouchStart(e) {
+    touchStart = {
+        x: e.changedTouches[0].clientX,
+        y: e.changedTouches[0].clientY
+    }
+}
+
+function onTouchEnd(e) {
+    const dx = e.changedTouches[0].clientX - touchStart.x
+    const dy = e.changedTouches[0].clientY - touchStart.y
+    if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
+        if (dx > 0 && touchStart.x <= 80) {
+            setNavbar(true)
+        } else {
+            setNavbar(false)
+        }
+    }
+}
+
+function setNavbar(shouldBeOpen) {
+    if (shouldBeOpen) {
+
+        themeContainer.classList.add('sidebar-open')
+    } else {
+
+        themeContainer.classList.remove('sidebar-open')
+    }
+}
 
 function toggleNavbar() {
-    const themeContainer = document.getElementsByClassName("theme-container")[0];
+    const themeContainer = document.getElementsByClassName('theme-container')[0];
     if (themeContainer.classList.contains('sidebar-open')) {
-        themeContainer.classList.remove('sidebar-open')
+        setNavbar(false)
     } else {
-        themeContainer.classList.add('sidebar-open')
+        setNavbar(true)
     }
 }
 
 window.toggleNavbar = toggleNavbar
+
+const themeContainer = document.getElementsByClassName('theme-container')[0];
+themeContainer.addEventListener('touchstart', onTouchStart, false);
+themeContainer.addEventListener('touchend', onTouchEnd, false);
