5f41b66e8a513d12fd748f15f51cfb1baa7451a2
Arran <arran@arranfrance.com>
2019-11-02 16:53:40 +0000

    Update service worker

--- src/sw.js
+++ src/sw.js
@@ -1,17 +1,21 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
 if (!workbox) {
   console.error(`Boo! Workbox didn't load 😬`);
 }
+
+// This will be auto filled in the build step
+workbox.precaching.precacheAndRoute([]);
 
 workbox.routing.registerRoute(
   new RegExp('\/(page\/\d)?'),
   new workbox.strategies.NetworkFirst()
 );
 
-
-// This will be auto filled in the build step
-workbox.precaching.precacheAndRoute([]);
+workbox.routing.registerRoute(
+  new RegExp('/index.json'),
+  new workbox.strategies.StaleWhileRevalidate(),
+);
 
 workbox.routing.registerRoute(
   /\.(?:png|jpg|jpeg)$/,
